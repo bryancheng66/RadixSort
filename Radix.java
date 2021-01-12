c9a799a70d4d5fa9c499b5833e00bfe8fada27d2
bryancheng66 <bryancheng66@gmail.com>
2021-01-12 12:17:19 -0500

    make length

--- Radix.java
+++ Radix.java
@@ -2,4 +2,8 @@ public class Radix{
 	public static int nth(int n, int col){
 		return (int)((n / (Math.pow(10, col))) % 10);
 	}
+
+	public static int length(int n){
+		return (int)Math.log10(n) + 1;
+	}
 }
