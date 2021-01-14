c2427ed004ee4f92ab588602f592aa2fc58ade83
bryancheng66 <bryancheng66@gmail.com>
2021-01-14 01:27:24 -0500

    make radixSortSimple and radixSort

--- Radix.java
+++ Radix.java
@@ -11,9 +11,40 @@ public class Radix{
 		}
 	}
 
-	public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+	public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
 		for(int i = 0; i < buckets.length; i++){
 			original.extend(buckets[i]);
 		}
+	}
+
+	public static void radixSortSimple(SortableLinkedList data){
+		int digits = 0;
+		SortableLinkedList[] buckets = new SortableLinkedList[10];
+
+		for(int i = 0; i < data.size(); i++){
+			if(length(data.get(i)) > digits){
+				digits = length(data.get(i));
+			}
+
+			buckets[nth(data.get(i), 0)].add(data.get(i));
+		}
+
+		data = new SortableLinkedList();
+		merge(data, buckets);
+
+		for(int i = 1; i < digits; i++){
+			for(int j = 0; i < data.size(); i++){
+				if(length(data.get(j)) > digits){
+					digits = length(data.get(j));
+				}
+
+				buckets[nth(data.get(j), i)].add(data.get(j));
+			}
+			data = new SortableLinkedList();
+			merge(data, buckets);
+		}
+	}
+
+	public static void radixSort(SortableLinkedList data){
 	}
 }
